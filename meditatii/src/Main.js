0c139fa4086488567a1bff9c733932efd90f8d9b
AndreiDruga <75364827+AndreiDruga@users.noreply.github.com>
2022-11-06 23:44:52 +0200

    create dots

--- meditatii/src/Main.js
+++ meditatii/src/Main.js
@@ -1,15 +1,45 @@
 import Header from "./components/Header/Header";
 import "./main.scss";
 
-
 export default function App() {
   return (
     <div>
-      <Header />
+      <section className="scroll-s-one" data-section-name="s-one">
+        <div className="containerS">
+          <div className="row">
+            <div className="col-lg-12">
+              <p>section one</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="scroll-s-two" data-section-name="s-two">
+        <div className="containerS">
+          <div className="row">
+            <div className="col-lg-12">
+              <p>section two</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="scroll-s-three" data-section-name="s-three">
+        <div className="containerS">
+          <div className="row">
+            <div className="col-lg-12">
+              <p>section three</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <div className="containerM">
-      <img className="image" src="src\german.png" />
+        <img src="https://picsum.photos/200/300" />
       </div>
-      
+      <div className="scroll-control">
+        <span className="one"></span>
+        <span className="two"></span>
+        <span className="three"></span>
+      </div>
     </div>
   );
 }
